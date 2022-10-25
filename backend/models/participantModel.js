a6ce2f26b4009360d8d2659b71ea3765da9e2df9
SahanDeF <82874443+SahanDeF@users.noreply.github.com>
2022-10-25 13:32:04 +0530

    participant Model

---
+++ backend/models/participantModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const participantSchema = new mongoose.Schema({
+    eventName:{
+        type:String,
+        require:true
+    },
+
+    participant: new mongoose.Schema({
+        name:{
+          type:String
+          require:true},
+        email:{
+          type:String
+          require:false}
+
+
+    })
+})
+
+module.exports = mongoose.model('task',participantSchema)
