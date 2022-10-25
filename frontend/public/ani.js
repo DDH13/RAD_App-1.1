e9587cf16706e22a76ada0a5565fbef2e1ab913d
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2022-10-25 22:11:34 +0530

    Add files via upload

---
+++ frontend/public/ani.js
@@ -0,0 +1,15 @@
+  // Requests the API for emails
+//   url = `/emails/${mailbox}`
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(emails => {
+//       // Print emails
+//       console.log(emails);
+//       displayemails(emails, mailbox)
+//     });
+
+// function viewtodolist()
+
+function submitd(res){
+    console.log(res)
+}
