a52eb36597c9cd59f3ad3adbd0563e2f0c0e688c
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2022-10-27 00:01:07 +0530

    Create Dashboard.js

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+  function dashboard() {
+    return (
+      <>
+      <div id="dashboard">
+
+        <div id="tdl"><h1>To-Do List</h1></div>
+        <div id="pp"><h1>Participant Planner</h1></div>
+        <div id="el"><h1>Event List</h1></div>
+        <div id="rp"><h1>Resource-People</h1></div>
+
+      </div>
+
+      </>
+    );
+  }
+
+  export default dashboard;
