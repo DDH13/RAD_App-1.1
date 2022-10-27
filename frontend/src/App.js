1b918c95c712e34cf7baf282abf82f36209e7b02
Dineth Hettiarachchi <63235220+DDH13@users.noreply.github.com>
2022-10-27 07:43:34 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,36 @@
 import { useEffect, useState } from "react";
-import Dashboard from "./components/Dashboard.js";
-import MyForm from "./components/TaskForm.js";
-import TaskList from "./components/TaskList.js";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
+import Login from "./Pages/Login.js";
+import Register from "./Pages/Register.js";
+import Home from "./Pages/Home.js";
+import TodoList from "./Pages/TodoList.js";
+import EventTracker from "./Pages/EventTracker.js";
+import ResourcePeople from "./Pages/ResourcePeople.js";
+import GuestPlanner from "./Pages/GuestPlanner.js";
 
 
 
 function App() {
 
-return (<>
-  <Dashboard />
-  <TaskList />
-  <MyForm/>
-</>
-)
+  return (<>
+    <Router>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/todo" element={<TodoList/>}/>
+        <Route path = "/event" element={<EventTracker/>}/>
+        <Route path = "/participant" element={<GuestPlanner/>}/>
+        <Route path = "/resourcepeople" element={<ResourcePeople/>}/>
+        <Route path= "/register" element ={<Register/>}/>
+        <Route path= "/login" element ={<Login/>}/>
 
+
+
+      </Routes>
+    </Router>
+
+  </>
+  )
 }
 
 export default App;
